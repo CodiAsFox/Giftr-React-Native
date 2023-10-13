8496a82f751b0d87d4102d6b0610081254282278
Tay F <rami0101@algonquinlive.com>
2023-10-13 18:28:34 -0400

    Expo Setup

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  Box,
+  Button,
+  ButtonText,
+  GluestackUIProvider,
+} from "@gluestack-ui/themed";
+import { useState } from "react";
+import { config } from "./config/gluestack-ui.config";
 export default function App() {
+  const [theme, setTheme] = useState("dark");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider colorMode={theme} config={config}>
+      <Box
+        justifyContent="center"
+        alignItems="center"
+        h="100%"
+        sx={{
+          _dark: {
+            bg: "$light900",
+          },
+          _light: {
+            bg: "$light200",
+          },
+        }}
+      >
+        <Button
+          sx={{
+            _dark: {
+              bg: "$red400",
+            },
+            _light: {
+              bg: "$red600",
+            },
+          }}
+        >
+          <ButtonText>Hello world</ButtonText>
+        </Button>
+        <Button
+          onPress={() => {
+            setTheme(theme == "light" ? "dark" : "light");
+          }}
+        >
+          <ButtonText>Toggle Theme</ButtonText>
+        </Button>
+      </Box>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
