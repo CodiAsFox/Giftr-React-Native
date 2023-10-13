4ae130bae80eecb70de26caae60f3ba94b6f4160
Taylor Ramirez <tay@vedla.io>
2023-10-13 04:07:07 +0000

    feat: Update React Native and Expo versions, add CodeSandbox configuration
    Update packages, styles, and configuration files in a React Native project.

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.h1}>Hello CodeSandbox</Text>
+        <Text style={styles.h2}>
+          Start editing to see some magic happen, even on your mobile device!
+        </Text>
+        <br />
+        <br />
+        <Text style={styles.paragraph}>
+          Open Expo on your mobile device with scanning the QR code in the
+          application log under the start tab.
+        </Text>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 8,
+    fontSize: 16,
+    textAlign: "center",
+  },
+  h1: {
+    margin: 28,
+    fontSize: 36,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  h2: {
+    margin: 16,
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
